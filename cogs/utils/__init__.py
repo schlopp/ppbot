82e29e115f677968667065d1882f03b85b746066
schlopp <god.tcs@gmail.com>
2022-11-20 18:20:18 +0100

    update __init__.py

--- cogs/utils/__init__.py
+++ cogs/utils/__init__.py
@@ -1,4 +1,4 @@
-from .types import Object
+from .helpers import Object, DifferenceTracker
 from .typehinted_methods import get_database_connection
 from .inventory import fetch_inventory, fetch_inventory_item_amount
 from .pps import PpRecordNotFoundError, Pp
