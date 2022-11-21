861d451fb873ffcea925ea2ba2fb3813417befc8
schlopp <god.tcs@gmail.com>
2022-11-21 23:34:19 +0100

    add database wrapper object

--- cogs/utils/helpers.py
+++ cogs/utils/helpers.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 from typing import Generic, TypeVar
 
 
-_T_co = TypeVar("_T_co", covariant=True)
+_IntStrT_co = TypeVar("_IntStrT_co", str, int, covariant=True)
 
 
 class Object:
@@ -16,40 +16,86 @@ class Object:
         )
         return f"<{self.__class__.__name__}{' ' if attribute_text else ''}{attribute_text}>"
 
-    def pretty_print(self, *, indent: int = 4, prefix: str = "", indent_level: int = 0, attribute_name: str | None = None):
+    def pretty_print(
+        self,
+        *,
+        indent: int = 4,
+        prefix: str = "",
+        indent_level: int = 0,
+        attribute_name: str | None = None,
+    ):
         if not self._repr_attributes or not indent:
             print(repr(self))
             return
 
         full_prefix = (prefix + " " * (indent - len(prefix))) * indent_level
         added_full_prefix = (prefix + " " * (indent - len(prefix))) * (indent_level + 1)
-        print(f"{full_prefix}{'' if attribute_name is None else attribute_name + '='}<{self.__class__.__name__}")
+        print(
+            f"{full_prefix}{'' if attribute_name is None else attribute_name + '='}<{self.__class__.__name__}"
+        )
         for attribute_name in self._repr_attributes:
             attribute = getattr(self, attribute_name)
             if isinstance(attribute, Object):
                 attribute.pretty_print(
-                    prefix=prefix, indent=indent, indent_level=indent_level + 1, attribute_name=attribute_name
+                    prefix=prefix,
+                    indent=indent,
+                    indent_level=indent_level + 1,
+                    attribute_name=attribute_name,
                 )
                 continue
             print(f"{added_full_prefix}{attribute_name}={repr(attribute)}")
         print(f"{full_prefix}>")
 
 
-class DifferenceTracker(Object, Generic[_T_co]):
+class DatabaseWrapperObject:
+    _trackers: tuple[str, ...] = ()
+    _table: str
+
+    def _generate_pgsql_set_query(
+        self, argument_position: int = 1
+    ) -> None | tuple[str, list[int | str]]:
+        update_values = []
+        query_arguments = []
+        for tracker_attribute_name in self._trackers:
+            tracker = getattr(self, tracker_attribute_name)
+            if not isinstance(tracker, DifferenceTracker):
+                raise TypeError(
+                    f"Expected atrribute {tracker_attribute_name!r} to be of type DifferenceTracker, "
+                    f"got {type(tracker)!r}"
+                )
+            if tracker.difference is None:
+                continue
+            elif isinstance(tracker.difference, int | str):
+                update_values.append(f"{tracker.column}=${argument_position}")
+            else:
+                raise TypeError(
+                    f"Expected DifferenceTracker {tracker!r}'s value to be of type int or str, "
+                    f"got {type(tracker.value)!r}"
+                )
+            query_arguments.append(tracker.value)
+            argument_position += 1
+        return (
+            (f"SET {', '.join(update_values)}", query_arguments)
+            if update_values
+            else None
+        )
+
+
+class DifferenceTracker(Object, Generic[_IntStrT_co]):
     __slots__ = ("value", "__start_value", "column")
     _repr_attributes = ("value", "start_value", "difference", "column")
 
-    def __init__(self, start_value: _T_co, *, column: str | None = None) -> None:
+    def __init__(self, start_value: _IntStrT_co, *, column: str | None = None) -> None:
         self.value = start_value
         self.__start_value = start_value
         self.column = column
 
     @property
-    def start_value(self) -> _T_co:
+    def start_value(self) -> _IntStrT_co:
         return self.__start_value
 
     @property
-    def difference(self) -> _T_co | None:
+    def difference(self) -> _IntStrT_co | None:
         if self.value == self.start_value:
             return None
         if isinstance(self.value, int):
