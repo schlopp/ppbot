33827d220e05d7d86b038b2c090c5e7f9aa74f07
schlopp <god.tcs@gmail.com>
2022-12-04 13:40:50 +0100

    remove old inventory handler

--- cogs/utils/inventory.py
+++ cogs/utils/inventory.py
@@ -9,46 +9,6 @@ import asyncpg  # type: ignore
 from . import DatabaseWrapperObject, DifferenceTracker, Record
 
 
-class _SupportsGetItem(Protocol):
-    def __getitem__(self, index):
-        ...
-
-
-async def fetch_inventory(
-    user_id: int,
-    connection: asyncpg.Connection,
-    *,
-    items: list[str] | None = None,
-    lock_for_update: bool = False,
-) -> dict[str, int]:
-    args = [user_id]
-    query = "SELECT item_name, amount FROM inventory WHERE user_id = $1"
-
-    if items is not None:
-        query = "+= AND item_name IN $2"
-        args.append(items)
-
-    if lock_for_update:
-        query += " FOR UPDATE"
-
-    rows = await connection.fetch(query, *args)
-    return {record["item_name"]: record["amount"] for record in rows}
-
-
-async def fetch_inventory_item_amount(
-    user_id: int,
-    item_name: str,
-    connection: asyncpg.Connection,
-    *,
-    lock_for_update: bool = False,
-) -> int:
-    query = "SELECT amount FROM inventory WHERE user_id = $1 AND item_name = $2"
-    if lock_for_update:
-        query += " FOR UPDATE"
-    amount = await connection.fetchval(query, user_id, item_name)
-    return 0 if amount is None else amount
-
-
 class InventoryItem(DatabaseWrapperObject):
     __slots__ = ("user_id", "name", "amount")
     _repr_attributes = __slots__
@@ -66,15 +26,6 @@ class InventoryItem(DatabaseWrapperObject):
         self.user_id = user_id
         self.name = name
         self.amount = DifferenceTracker(amount, column="item_amount")
-
-    @classmethod
-    async def fetch_all(
-        cls, connection: asyncpg.Connection, user_id: int
-    ) -> list[InventoryItem]:
-        records: list[Record] = connection.fetch(
-            "SELECT * FROM inventory WHERE user_id = $1", user_id
-        )
-        return [cls.from_record(record) for record in records]
 
     async def update(
         self, connection: asyncpg.Connection, ensure_difference: bool = True
