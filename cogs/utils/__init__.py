7742e7c4a2f60fda38d24c45eb7c20d32c6a5f14
schlopp <god.tcs@gmail.com>
2022-12-04 13:40:39 +0100

    add more helpers

--- cogs/utils/__init__.py
+++ cogs/utils/__init__.py
@@ -1,4 +1,18 @@
-from .helpers import Record, Object, DatabaseWrapperObject, DifferenceTracker
+from .helpers import (
+    RED,
+    GREEN,
+    BLUE,
+    PINK,
+    limit_text,
+    IntFormatType,
+    format_int,
+    compare,
+    find_nearest_number,
+    Record,
+    Object,
+    DatabaseWrapperObject,
+    DifferenceTracker,
+)
 from .typehinted_methods import get_database_connection
-from .inventory import fetch_inventory, fetch_inventory_item_amount, InventoryItem
+from .inventory import InventoryItem
 from .pps import PpRecordNotFoundError, Pp
--- cogs/utils/helpers.py
+++ cogs/utils/helpers.py
@@ -1,9 +1,13 @@
 from __future__ import annotations
+import enum
+import math
+import random
 import sys
-from collections.abc import Mapping
-from typing import Generic, TypeVar, Any
+from collections.abc import Mapping, Iterable
+from typing import Generic, TypeVar, Any, Literal, overload
 
 import asyncpg  # type: ignore
+import discord
 
 if sys.version_info >= (3, 11):
     from typing import Self
@@ -15,6 +19,73 @@ _IntStrT_co = TypeVar("_IntStrT_co", str, int, covariant=True)
 
 
 Record = Mapping[str, Any]
+
+
+RED = discord.Colour(16007990)
+GREEN = discord.Colour(5025616)
+BLUE = discord.Colour(2201331)
+PINK = discord.Colour(15418782)
+UNITS = ["million", "billion", "trillion", "quadrillion"]
+
+
+def limit_text(text: str, limit: int):
+    assert limit >= 0
+    if limit == 1:
+        return text[0]
+    if len(text) - 1 > limit:
+        return text[0 : limit - 1] + "\N{HORIZONTAL ELLIPSIS}"
+    return text
+
+
+def compare(x: int, y: int) -> int:
+    if x < y:
+        return -1
+    if x == y:
+        return 0
+    return 1
+
+
+def find_nearest_number(
+    numbers: Iterable[int], number: int
+) -> tuple[int, Literal[-1, 0, 1]]:
+    nearest_number: int | None = None
+    for i in numbers:
+        if i > number:
+            if nearest_number is not None:
+                match compare(abs(nearest_number - number), abs(i - number)):
+                    case -1:
+                        return nearest_number, -1
+                    case 0:
+                        return random.choice([(nearest_number, -1), (i, 1)])
+                    case _:
+                        return i, 1
+            return i
+        if i == number:
+            return i, 0
+        nearest_number = i
+    return nearest_number, -1
+
+
+class IntFormatType(enum.Enum):
+    FULL = enum.auto()
+    FULL_UNIT = enum.auto()
+    ABBREVIATED_UNIT = enum.auto()
+
+
+def format_int(value: int, format_type: IntFormatType = IntFormatType.FULL_UNIT) -> str:
+    if format_type == IntFormatType.FULL or -(10**6) < value < 10**6:
+        return f"{value:,}"
+    else:
+        unit = math.floor(math.log10(value + 1 if not value else abs(value))) // 3
+        unit_value = math.floor(value / 10 ** (unit * 3) * 100) / 100
+
+        if unit_value.is_integer():
+            unit_value = math.floor(unit_value)
+
+        if format_type == IntFormatType.FULL_UNIT:
+            return f"{unit_value} {UNITS[unit - 2]}"
+
+        return f"{unit_value}{UNITS[unit - 2][0].upper()}"
 
 
 class Object:
@@ -102,7 +173,7 @@ class DatabaseWrapperObject(Object):
         )
 
     @classmethod
-    def _generate_static_pgsql_where_query(
+    def _generate_cls_pgsql_where_query(
         cls, required_values: dict[str, Any], *, argument_position: int = 1
     ) -> tuple[str, list[str], int]:
         conditional_values: list[str] = []
@@ -141,26 +212,109 @@ class DatabaseWrapperObject(Object):
         )
 
     @classmethod
+    def _generate_cls_pgsql_select_query(
+        cls, selected_columns: Iterable[str] | None = None
+    ) -> str:
+        columns = selected_columns if selected_columns is not None else "*"
+        return f"SELECT {', '.join(columns)}"
+
+    @classmethod
     def from_record(cls: type[Self], record: Record) -> Self:
         return cls(**{cls._columns[column]: value for column, value in record.items()})
 
+    @overload
     @classmethod
-    async def fetch(
-        cls,
+    async def fetch_record(
+        cls: type[Self],
         connection: asyncpg.Connection,
         required_values: dict[str, Any],
+        selected_columns: Iterable[str] | None = None,
+        *,
+        fetch_multiple_rows: bool = True,
+    ) -> list[Record] | None:
+        ...
+
+    @overload
+    @classmethod
+    async def fetch_record(
+        cls: type[Self],
+        connection: asyncpg.Connection,
+        required_values: dict[str, Any],
+        selected_columns: Iterable[str] | None = None,
         *,
         lock_for_update: bool = False,
-    ):
-        where_query, where_query_arguments, _ = cls._generate_static_pgsql_where_query(
+    ) -> Record | None:
+        ...
+
+    @classmethod
+    async def fetch_record(
+        cls: type[Self],
+        connection: asyncpg.Connection,
+        required_values: dict[str, Any],
+        selected_columns: Iterable[str] | None = None,
+        *,
+        lock_for_update: bool = False,
+        fetch_multiple_rows: bool = False,
+    ) -> Record | list[Record] | None:
+        where_query, where_query_arguments, _ = cls._generate_cls_pgsql_where_query(
             required_values
         )
-        query = f"SELECT * FROM {cls._table} {where_query}"
+        query = f"{cls._generate_cls_pgsql_select_query(selected_columns)} FROM {cls._table} {where_query}"
 
         if lock_for_update:
             query += " FOR UPDATE"
 
+        if fetch_multiple_rows:
+            rows: list[Record] = await connection.fetch(query, *where_query_arguments)
+            if not rows:
+                return None
+            return rows
         record: Record = await connection.fetchrow(query, *where_query_arguments)
+        if record is None:
+            return None
+        return record
+
+    @overload
+    @classmethod
+    async def fetch(
+        cls: type[Self],
+        connection: asyncpg.Connection,
+        required_values: dict[str, Any],
+        *,
+        fetch_multiple_rows: Literal[True],
+    ) -> list[Self] | None:
+        ...
+
+    @overload
+    @classmethod
+    async def fetch(
+        cls: type[Self],
+        connection: asyncpg.Connection,
+        required_values: dict[str, Any],
+        *,
+        lock_for_update: Literal[True],
+    ) -> Self | None:
+        ...
+
+    @classmethod
+    async def fetch(
+        cls: type[Self],
+        connection: asyncpg.Connection,
+        required_values: dict[str, Any],
+        *,
+        lock_for_update: bool = False,
+        fetch_multiple_rows: bool = False,
+    ) -> Self | list[Self] | None:
+        if fetch_multiple_rows:
+            records = await cls.fetch_record(
+                connection, required_values, fetch_multiple_rows=True
+            )
+            if not records:
+                return None
+            return [cls.from_record(record) for record in records]
+        record: Record = await cls.fetch_record(
+            connection, required_values, lock_for_update=True
+        )
         if record is None:
             return None
 
