b45d3ddc900b93525285ff1321f1cb861420fc2a
schlopp <god.tcs@gmail.com>
2023-10-12 15:43:00 +0200

    Typestubs for rust_utils

---
+++ rust_utils.pyi
@@ -0,0 +1,8 @@
+"""
+Utility module written in Rust using PyO3 and Maturin. Typestubs maintained manually.
+
+
+Type conversions for Rust (PyO3) -> Python: https://pyo3.rs/v0.19.2/conversions/tables
+"""
+
+__all__ = []
