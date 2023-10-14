47fed2c177ca590ad42a0bc2edb9185f5ab191d4
schlopp <god.tcs@gmail.com>
2023-10-15 00:32:07 +0200

    Add compute_multiplier_item_cost to rust utils

--- rust-src/lib.rs
+++ rust-src/lib.rs
@@ -1,6 +1,27 @@
+// use num_traits::Zero;
 use pyo3::prelude::*;
 
+#[pyfunction]
+fn compute_multiplier_item_cost(
+    amount: usize,
+    current_multiplier: usize,
+    item_cost: usize,
+    item_gain: usize,
+) -> (usize, usize) {
+    let cost = ((current_multiplier..=amount + current_multiplier - 1)
+        .map(|x| (x as f64).powf(1.3))
+        .sum::<f64>()
+        * (item_cost as f64))
+        .floor() as usize;
+
+    let gain = item_gain * amount;
+
+    (cost, gain)
+}
+
 #[pymodule]
-fn rust_utils(_py: Python, _m: &PyModule) -> PyResult<()> {
+fn rust_utils(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(compute_multiplier_item_cost, m)?)
+        .unwrap();
     Ok(())
 }
--- rust_utils.pyi
+++ rust_utils.pyi
@@ -6,3 +6,11 @@ Type conversions for Rust (PyO3) -> Python: https://pyo3.rs/v0.19.2/conversions/
 """
 
 __all__ = []
+
+def compute_multiplier_item_cost(
+    amount: int, current_multiplier: int, item_cost: int, item_gain: int
+) -> tuple[int, int]:
+    """
+    Computes the cost of buying a certain amount of a multiplier item, along with the gain it'll
+    give. Returns tuple `(cost, gain)`.
+    """
