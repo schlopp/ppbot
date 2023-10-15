6150b7a0a17c269af9139bd99ccc401acb90f168
schlopp <god.tcs@gmail.com>
2023-10-15 20:18:30 +0200

    Add PyO3 computing function for /buy max usage

--- rust-src/lib.rs
+++ rust-src/lib.rs
@@ -19,9 +19,50 @@ fn compute_multiplier_item_cost(
     (cost, gain)
 }
 
+#[pyfunction]
+fn compute_max_multiplier_item_purchase_amount(
+    available_inches: usize,
+    current_multiplier: usize,
+    item_cost: usize,
+    item_gain: usize,
+) -> (usize, usize, usize) {
+    let mut max_reached = false;
+    let mut min_amount = 0usize;
+    let mut max_amount = 10usize;
+    let mut amount = max_amount;
+
+    loop {
+        let old_amount = amount;
+        amount = min_amount + ((max_amount as f64 - min_amount as f64) / 2.0).floor() as usize;
+
+        let (cost, gain) =
+            compute_multiplier_item_cost(amount, current_multiplier, item_cost, item_gain);
+
+        if amount == old_amount {
+            return (amount, cost, gain);
+        }
+
+        if cost > available_inches {
+            max_amount = amount;
+            max_reached = true;
+        } else {
+            min_amount = amount;
+
+            if !max_reached {
+                max_amount *= 2;
+            }
+        }
+    }
+}
+
 #[pymodule]
 fn rust_utils(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(compute_multiplier_item_cost, m)?)
         .unwrap();
+    m.add_function(wrap_pyfunction!(
+        compute_max_multiplier_item_purchase_amount,
+        m
+    )?)
+    .unwrap();
     Ok(())
 }
--- rust_utils.pyi
+++ rust_utils.pyi
@@ -14,3 +14,14 @@ def compute_multiplier_item_cost(
     Computes the cost of buying a certain amount of a multiplier item, along with the gain it'll
     give. Returns tuple `(cost, gain)`.
     """
+
+def compute_max_multiplier_item_purchase_amount(
+    available_inches: int,
+    current_multiplier: int,
+    item_cost: int,
+    item_gain: int,
+) -> tuple[int, int, int]:
+    """
+    Computes the maximum purchasable amount of a multiplier item, along with the cost and the gain
+    it'll give. Returns tuple `(amount, cost, gain)`.
+    """
