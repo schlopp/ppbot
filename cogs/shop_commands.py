24856ff369f09228a259262540bc98d6613b5eb4
schlopp <god.tcs@gmail.com>
2023-10-20 00:43:54 +0200

    Add `/buy <item> (max | all)` using Rust PyO3 computing

--- cogs/shop_commands.py
+++ cogs/shop_commands.py
@@ -234,7 +234,7 @@ class ShopCommandCog(vbu.Cog[utils.Bot]):
                 ),
                 discord.ApplicationCommandOption(
                     name="amount",
-                    type=discord.ApplicationCommandOptionType.integer,
+                    type=discord.ApplicationCommandOptionType.string,
                     description="How many of this item u wanna buy",
                 ),
             ]
@@ -242,13 +242,22 @@ class ShopCommandCog(vbu.Cog[utils.Bot]):
     )
     @commands.is_slash_command()
     async def buy_command(
-        self, ctx: commands.SlashContext[utils.Bot], item: str, amount: int
+        self, ctx: commands.SlashContext[utils.Bot], item: str, amount: str
     ) -> None:
-        if amount < 1:
-            raise commands.CheckFailure("You can't buy less than one of an item!")
+        if amount not in {"all", "max"} and not (
+            amount.isnumeric() and int(amount) >= 1
+        ):
+            raise commands.CheckFailure(
+                f"{utils.clean(amount)!r} is not a valid amount!"
+                " Please enter `all`, `max`, or a (positive) number"
+            )
 
-        async with utils.DatabaseWrapper() as db, db.transaction(), utils.DatabaseTimeoutManager.notify(
-            ctx.author.id, "You're still busy buying an item!"
+        async with (
+            utils.DatabaseWrapper() as db,
+            db.transaction(),
+            utils.DatabaseTimeoutManager.notify(
+                ctx.author.id, "You're still busy buying an item!"
+            ),
         ):
             try:
                 pp = await utils.Pp.fetch_from_user(db.conn, ctx.author.id, edit=True)
@@ -266,7 +275,7 @@ class ShopCommandCog(vbu.Cog[utils.Bot]):
                 embed.title = "Purchase failed: Unknown item"
                 embed.description = (
                     f"The item {utils.clean(item)!r} doesn't exist!"
-                    " Make sure you select one of the options or spell the item's name correctly."
+                    " Make sure you select one of the options or spell the item's name correctly"
                 )
 
                 buttons = []
@@ -372,32 +381,64 @@ class ShopCommandCog(vbu.Cog[utils.Bot]):
                     await ctx.interaction.response.send_message(**kwargs)
 
             embed = utils.Embed()
-            if isinstance(item_object, utils.MultiplierItem):
-                if amount > self.MAX_MULTIPLIER_PURCHASE_AMOUNT:
-                    embed.colour = utils.RED
-                    embed.title = f"Purchase failed - buying too much shit at once"
-                    embed.description = (
-                        f"You can't buy more than {utils.format_int(self.MAX_MULTIPLIER_PURCHASE_AMOUNT)}"
-                        f" multipliers at once, sorry!"
+
+            if amount in {"all", "max"}:
+                if isinstance(item_object, utils.MultiplierItem):
+                    amount_number, cost, gain = item_object.compute_max_purchase(
+                        available_inches=pp.size.value,
+                        current_multiplier=pp.multiplier.value,
                     )
-                    embed.add_tip()
+                    if amount_number < 1:
+                        embed.colour = utils.RED
+                        embed.title = f"Purchase failed - ur broke"
 
-                    await responder(embed=embed, components=None, content=None)
-                    return
+                        cost, _ = item_object.compute_cost(
+                            1, current_multiplier=pp.multiplier.value
+                        )
+                        embed.description = (
+                            f"You literally can't even afford {item_object.format_amount(1)}"
+                            f" lmao. Come back when you have"
+                            f" **{utils.format_int(cost - pp.size.value)}** more inches loser"
+                        )
+                        embed.add_tip()
 
-                price, gain = item_object.compute_cost(
-                    amount, current_multiplier=pp.multiplier.value
-                )
+                        await responder(embed=embed, components=None, content=None)
+                        return
+                else:
+                    amount_number = pp.size.value // item_object.price
+                    cost = amount_number * item_object.price
+                    gain = None
             else:
-                price = item_object.price * amount
-                gain = None
+                amount_number = int(amount)
+                if isinstance(item_object, utils.MultiplierItem):
+                    if amount_number > self.MAX_MULTIPLIER_PURCHASE_AMOUNT:
+                        embed.colour = utils.RED
+                        embed.title = (
+                            f"Purchase failed - buying too many multipliers at once"
+                        )
+                        embed.description = (
+                            f"You can't buy more than"
+                            f" {utils.format_int(self.MAX_MULTIPLIER_PURCHASE_AMOUNT)} multipliers"
+                            f" at once, sorry!"
+                        )
+                        embed.add_tip()
 
-            if price > pp.size.value:
+                        await responder(embed=embed, components=None, content=None)
+                        return
+                    cost, gain = item_object.compute_cost(
+                        amount_number, current_multiplier=pp.multiplier.value
+                    )
+                else:
+                    cost = amount_number * item_object.price
+                    gain = None
+
+            if cost > pp.size.value:
                 embed.colour = utils.RED
                 embed.title = f"Purchase failed - ur broke"
                 embed.description = (
-                    f"You need {pp.format_growth(price - pp.size.value)}"
-                    f" more to afford {item_object.format_amount(amount)}"
+                    f"You need {pp.format_growth(cost - pp.size.value)}"
+                    f" more to afford {item_object.format_amount(amount_number)}"
+                    " <:Sadge:1076202349246291978>"
                 )
                 embed.add_tip()
 
@@ -405,10 +446,13 @@ class ShopCommandCog(vbu.Cog[utils.Bot]):
                 return
 
             embed.colour = utils.BLUE
-            embed.title = f"Buying {item_object.format_amount(amount, markdown=None)}"
+            embed.title = (
+                f"Buying {item_object.format_amount(amount_number, markdown=None)}"
+            )
             embed.description = (
-                f"Are you sure that you want to buy {item_object.format_amount(amount)}"
-                f" for {pp.format_growth(price)}?"
+                f"Are you sure that you want to buy {item_object.format_amount(amount_number)}"
+                f" for {pp.format_growth(cost)}? You'll have"
+                f" {pp.format_growth(pp.size.value - cost)} left"
                 f"\n\n{self.format_listing(item_object, pp=pp)}"
             )
 
@@ -452,26 +496,26 @@ class ShopCommandCog(vbu.Cog[utils.Bot]):
                 embed.colour = utils.RED
                 embed.title = "Purchase cancelled"
                 embed.description = (
-                    f"You've cancelled your purchase of {item_object.format_amount(amount)}"
-                    f" for {pp.format_growth(price)}"
+                    f"You've cancelled your purchase of {item_object.format_amount(amount_number)}"
+                    f" for {pp.format_growth(cost)}"
                 )
                 await component_interaction.response.edit_message(
                     embed=embed, components=None
                 )
                 return
 
-            pp.size.value -= price
+            pp.size.value -= cost
             embed.colour = utils.GREEN
             embed.title = "Purchase successful"
 
             if gain is not None:
                 pp.multiplier.value += gain
                 embed.description = (
-                    f"*You take the {item_object.format_amount(amount)} for {pp.format_growth(price)}"
-                    " and feel a sudden surge of power coursing through your pp's veins."
-                    f" You gain an additional **+{utils.format_int(gain)}** multiplier."
-                    f"\n\nYou now have {pp.format_growth(price)}"
-                    f" and a **{utils.format_int(pp.multiplier.value)}x** multiplier!"
+                    f"*You take the {item_object.format_amount(amount_number)} for"
+                    f" {pp.format_growth(cost)} and feel a sudden surge of power coursing through"
+                    f" your pp's veins. You gain an additional **+{utils.format_int(gain)}**"
+                    f" multiplier.\n\nYou now have {pp.format_growth(cost)} and a"
+                    f" **{utils.format_int(pp.multiplier.value)}x** multiplier!"
                 )
             else:
                 try:
@@ -485,13 +529,14 @@ class ShopCommandCog(vbu.Cog[utils.Bot]):
                         ctx.author.id, item_object.id, 0
                     )
 
-                inventory_item.amount.value += amount
+                inventory_item.amount.value += amount_number
                 await inventory_item.update(db.conn)
 
                 embed.description = (
-                    f"You've successfully purchased {item_object.format_amount(amount)}"
-                    f" for {pp.format_growth(price)}. You now have {pp.format_growth(pp.size.value)}"
-                    f" and {item_object.format_amount(amount)}!"
+                    f"You've successfully purchased {item_object.format_amount(amount_number)}"
+                    f" for {pp.format_growth(cost)}. You now have"
+                    f" {pp.format_growth(pp.size.value)} and"
+                    f" {item_object.format_amount(inventory_item.amount.value)}!"
                 )
 
             await pp.update(db.conn)
