90c2753fe8f2032b6f04bb34611c646e3fa0908b
schlopp <god.tcs@gmail.com>
2024-05-26 20:28:01 +0200

    Add full help command

--- cogs/help_commands.py
+++ cogs/help_commands.py
@@ -1,13 +1,75 @@
+import random
+
+import discord
 from discord.ext import commands, vbu
 
 from . import utils
 
 
 class HelpCommandsCog(vbu.Cog[utils.Bot]):
+    QUOTES = [
+        "wtf is this bot",
+        "i love my wife",
+        "schlopp is SO HOT",
+        "my gf has a bigger pp than me",
+    ]
+
+    def generate_help_embed(self) -> utils.Embed:
+        embed = utils.Embed()
+        embed.color = utils.BLUE
+        embed.title = "u need some help?"
+        embed.url = utils.MEME_URL
+        embed.set_footer(text=f'"{random.choice(self.QUOTES)}"')
+
+        for category in utils.CommandCategory:
+            category_commands: list[commands.Command] = []
+            for command in self.bot.commands:
+                if command.hidden:
+                    continue
+
+                if not command.application_command_meta:
+                    continue
+
+                if isinstance(command, utils.Command):
+                    if command.category == category:
+                        category_commands.append(command)
+
+                elif category == utils.CommandCategory.OTHER:
+                    category_commands.append(command)
+
+            if not category_commands:
+                continue
+
+            category_commands.sort(key=lambda c: c.name)
+
+            text = " ".join(
+                utils.format_slash_command(command.name)
+                for command in category_commands
+            )
+            embed.add_field(name=category.value, value=text)
+
+        return embed
+
+    def generate_new_user_embed(self) -> utils.Embed:
+        embed = utils.Embed()
+        embed.color = utils.PINK
+
+        embed.set_author(name="IMPORTANT!!!!!1!!")
+        embed.title = "u dont have a pp yet!!!"
+        embed.url = f"{utils.MEME_URL}#"
+
+        embed.description = (
+            f"use {utils.format_slash_command('new')}"
+            " to make a pp and start growing it :)"
+        )
+
+        self.logger.info("1")
+        return embed
+
     @commands.command(
         "help",
         utils.Command,
-        category=utils.CommandCategory.GETTING_STARTED,
+        category=utils.CommandCategory.HELP,
         application_command_meta=commands.ApplicationCommandMeta(),
     )
     @commands.is_slash_command()
@@ -16,7 +78,16 @@ class HelpCommandsCog(vbu.Cog[utils.Bot]):
         Need some help? Here you go :)
         """
 
-        await ctx.interaction.response.send_message("no help for u")
+        embeds: list[discord.Embed] = [self.generate_help_embed()]
+
+        async with utils.DatabaseWrapper() as db:
+            try:
+                await utils.Pp.fetch_from_user(db.conn, ctx.author.id)
+            except utils.NoPpCheckFailure:
+                embeds.append(self.generate_new_user_embed())
+
+        self.logger.info(embeds)
+        await ctx.interaction.response.send_message(embeds=embeds)
 
 
 async def setup(bot: utils.Bot):
