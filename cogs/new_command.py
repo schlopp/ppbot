500d5ca68787e8e69b79fe1330bac3210c93b3b1
schlopp <god.tcs@gmail.com>
2024-05-27 11:47:49 +0200

    Add tutorial to /new

--- cogs/new_command.py
+++ cogs/new_command.py
@@ -25,13 +25,42 @@ class NewCommandCog(vbu.Cog[utils.Bot]):
                 await db("INSERT INTO pps VALUES ($1)", ctx.author.id)
                 embed.colour = utils.GREEN
                 embed.description = f"{ctx.author.mention}, you now have a pp!"
+                new_pp = True
             else:
                 embed.colour = utils.RED
                 embed.description = f"{ctx.author.mention}, you already have a pp!"
+                new_pp = False
 
             embed.add_tip()
 
             await ctx.interaction.response.send_message(embed=embed)
+
+            if not new_pp:
+                return
+
+            embed = utils.Embed()
+            embed.color = utils.PINK
+            embed.title = "welcome to pp bot!!"
+            embed.url = utils.MEME_URL
+            embed.description = (
+                "hello and welcome to the cock growing experience."
+                " you've just created your very own pp,"
+                " but right now it's only **0 inches** (small as fuck)"
+                "\n\n"
+                "to start growing ur pp,"
+                f" use {utils.format_slash_command('grow')} and {utils.format_slash_command('beg')}"
+                "\n\n"
+                "when ur pp gets big enough,"
+                f" you can go to the {utils.format_slash_command('shop')}"
+                " and buy yourself some pp-growing pills."
+                " these pills increase your multiplier,"
+                " which makes you get more inches when using commands :)"
+                "\n\n"
+                "eventually you'll be able to buy some items that unlock new pp-growing commands."
+                f" check {utils.format_slash_command('unlocked-commands')}"
+                " to see what items unlock which commands"
+            )
+            await ctx.interaction.followup.send(embed=embed, ephemeral=True)
 
 
 async def setup(bot: utils.Bot):
