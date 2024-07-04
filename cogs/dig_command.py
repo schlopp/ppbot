a0372435ec5e3c6ab21cf41b8c971e3be3a14641
schlopp <god.tcs@gmail.com>
2024-07-04 21:46:47 +0200

    add multiplier rewards and depth rewards

--- cogs/dig_command.py
+++ cogs/dig_command.py
@@ -1,4 +1,6 @@
 import enum
+import itertools
+import math
 import random
 from typing import Literal, cast
 
@@ -12,7 +14,8 @@ from . import utils
 
 
 class Activity(enum.Enum):
-    SUCCESS = 0.6
+    SUCCESS_GROW = 0.55
+    SUCCESS_MULTIPLIER = 0.05
     SHOVEL_BREAK = 0.3
     CLICK_THAT_BUTTON_MINIGAME = 0.1
 
@@ -21,6 +24,16 @@ class Activity(enum.Enum):
         return random.choices(
             list(Activity), weights=list(activity.value for activity in Activity)
         )[0]
+
+
+class DepthReward(enum.Enum):
+    COOL_BOX = ("cool box of stuff :)", "cool boxes of stuff :)")
+    TREASURE_CHEST = ("treasure chest!", "treasure chests!")
+    AWESOME_TREASURE_CHEST = (
+        "SUPER MEGA AWESOME TREASURE CHEST!!",
+        "SUPER MEGA AWESOME TREASURE CHESTS!!",
+    )
+    GIFT_FROM_THE_PP_GODS = ("gift from the pp gods", "gifts from the pp gods")
 
 
 MinigameActivity = Literal[Activity.CLICK_THAT_BUTTON_MINIGAME]
@@ -32,9 +45,108 @@ class DigCommandCog(vbu.Cog[utils.Bot]):
         "{}'s shovel literally snapped in half while trying to dig",
         "{}'s shovel shattered into a thousand pieces",
     ]
+    UNIQUE_DEPTH_REWARDS: dict[int, DepthReward] = {
+        69: DepthReward.GIFT_FROM_THE_PP_GODS,
+        420: DepthReward.GIFT_FROM_THE_PP_GODS,
+        666: DepthReward.GIFT_FROM_THE_PP_GODS,
+        6969: DepthReward.GIFT_FROM_THE_PP_GODS,
+    }
 
     def __init__(self, bot: Bot, logger_name: str | None = None):
         super().__init__(bot, logger_name)
+
+    def _get_depth_reward(self, depth: int) -> DepthReward | None:
+        try:
+            return self.UNIQUE_DEPTH_REWARDS[depth]
+        except:
+            pass
+
+        if depth == 10:
+            return DepthReward.COOL_BOX
+        elif depth % 1000 == 0:
+            return DepthReward.GIFT_FROM_THE_PP_GODS
+        elif depth % 100 == 0:
+            return DepthReward.AWESOME_TREASURE_CHEST
+        elif depth % 25 == 0:
+            return DepthReward.TREASURE_CHEST
+
+    def _get_depth_rewards(self, depth_range: range) -> dict[int, DepthReward] | None:
+        rewards: dict[int, DepthReward] = {}
+
+        for depth in depth_range:
+            try:
+                rewards[depth] = self.UNIQUE_DEPTH_REWARDS[depth]
+            except:
+                pass
+
+            if depth == 10:
+                rewards[depth] = DepthReward.COOL_BOX
+            elif depth % 1000 == 0:
+                rewards[depth] = DepthReward.GIFT_FROM_THE_PP_GODS
+            elif depth % 100 == 0:
+                rewards[depth] = DepthReward.AWESOME_TREASURE_CHEST
+            elif depth % 25 == 0:
+                rewards[depth] = DepthReward.TREASURE_CHEST
+
+        if not rewards:
+            return None
+
+        return dict(sorted(rewards.items()))
+
+    def _generate_reward_visual(self, pp: utils.Pp) -> str:
+        unformatted_segments: list[tuple[str, str]] = []
+
+        if pp.digging_depth.value % 5 == 0:
+            closest_standard_depth = pp.digging_depth.value + 5
+        else:
+            closest_standard_depth = math.ceil(pp.digging_depth.value / 5) * 5
+        closest_depth = closest_standard_depth
+        future_rewards: dict[int, DepthReward] = {}
+
+        rewards_before_closest_depth = self._get_depth_rewards(
+            range(pp.digging_depth.value + 1, closest_standard_depth)
+        )
+        if rewards_before_closest_depth:
+            closest_depth = next(iter(rewards_before_closest_depth))
+            future_rewards.update(rewards_before_closest_depth)
+
+        for depth in itertools.count(closest_depth):
+            if len(unformatted_segments) >= 6:
+                break
+
+            depth_segment = utils.format_int(
+                depth, format_type=utils.IntFormatType.FULL
+            )
+            depth_segment = f"{depth_segment} ft"
+            if depth in future_rewards:
+                segment = f"{{}} [{future_rewards[depth].value[0]}]"
+                unformatted_segments.append((depth_segment, segment))
+                continue
+
+            reward = self._get_depth_reward(depth)
+            if reward:
+                segment = f"{{}} [{reward.value[0]}]"
+                unformatted_segments.append((depth_segment, segment))
+                continue
+
+            if depth % 5 == 0:
+                segment = f"{{}} ..."
+                unformatted_segments.append((depth_segment, segment))
+                continue
+
+        max_depth_segment_len = max(
+            len(depth_segment) for depth_segment, _ in unformatted_segments
+        )
+        segments = [
+            segment.format(f"{depth_segment:<{max_depth_segment_len}}")
+            for depth_segment, segment in unformatted_segments
+        ]
+
+        formatted_depth = utils.format_int(
+            pp.digging_depth.value, format_type=utils.IntFormatType.FULL
+        )
+        segment = f"{f'{formatted_depth} ft':<{max_depth_segment_len}} (you are here!)"
+        return f"```css\n{segment}\n\n" + "\n".join(segments) + "```"
 
     async def start_minigame(
         self,
@@ -105,18 +217,78 @@ class DigCommandCog(vbu.Cog[utils.Bot]):
 
             embeds = []
 
-            if activity in {Activity.SUCCESS, Activity.SHOVEL_BREAK}:
+            if activity in {
+                Activity.SUCCESS_GROW,
+                Activity.SUCCESS_MULTIPLIER,
+                Activity.SHOVEL_BREAK,
+            }:
                 embed = utils.Embed()
                 embeds.append(embed)
 
-                growth = random.randint(1, 15)
-                pp.grow_with_multipliers(growth)
+                depth = random.randint(1, 3 * 100)
+                pp.digging_depth.value += depth
 
-                embed.colour = utils.GREEN
                 embed.description = (
-                    f"**{ctx.author.mention}** went digging"
-                    f" and found {pp.format_growth()}!"
+                    f"**{ctx.author.mention}** dug another {utils.format_int(depth)} feet down"
+                    f" and found {{}}"
                 )
+
+                if activity in {Activity.SUCCESS_GROW, Activity.SHOVEL_BREAK}:
+                    growth = random.randint(30, 60)
+                    pp.grow_with_multipliers(growth, voted=await pp.has_voted())
+
+                    embed.colour = utils.GREEN
+                    embed.description = embed.description.format(
+                        f"{pp.format_growth()}!"
+                    )
+
+                elif activity == Activity.SUCCESS_MULTIPLIER:
+                    increase = random.choices(
+                        [5, 4, 3, 2, 1],
+                        [1, 2, 4, 8, 16],
+                        k=1,
+                    )[0]
+                    pp.multiplier.value += increase
+
+                    embed.colour = utils.PINK
+                    embed.description = embed.description.format(
+                        f" a [**{utils.format_int(increase)}x multiplier!!!**]({utils.MEME_URL})"
+                    )
+
+                new_rewards = self._get_depth_rewards(
+                    range(pp.digging_depth.start_value + 1, pp.digging_depth.value + 1)
+                )
+
+                embed.description += (
+                    "\n\n<:shovel:1258091579843809321> You've dug"
+                    f" **{utils.format_int(pp.digging_depth.value)}** feet deep"
+                )
+
+                if new_rewards:
+                    new_rewards_compiled: dict[DepthReward, int] = {}
+                    for reward in new_rewards.values():
+                        try:
+                            new_rewards_compiled[reward] += 1
+                        except KeyError:
+                            new_rewards_compiled[reward] = 1
+
+                    segments = [
+                        utils.format_amount(
+                            *reward.value,
+                            amount,
+                            markdown=utils.MarkdownFormat.BOLD_BLUE,
+                            full_markdown=True,
+                        )
+                        for reward, amount in new_rewards_compiled.items()
+                    ]
+                    embed.description += (
+                        f" and found {utils.format_iterable(segments, inline=True)}"
+                    )
+
+                embed.description += (
+                    f". Here are your next rewards:\n{self._generate_reward_visual(pp)}"
+                )
+
                 embed.add_tip()
 
                 if activity == Activity.SHOVEL_BREAK:
